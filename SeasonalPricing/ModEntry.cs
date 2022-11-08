1940ab70c3bd4396c815d759f6e25f296394fc23
jaerockets <jae.rockets@gmail.com>
2022-11-07 20:19:14 -0800

    finished

--- SeasonalPricing/ModEntry.cs
+++ SeasonalPricing/ModEntry.cs
@@ -30,7 +30,12 @@ namespace SeasonalPricing
                 {
                     this.Monitor.Log("lambda execute", LogLevel.Debug);
                     var data = asset.AsDictionary<int, string>().Data;
-                    List<int> springCrops = new List<int> { 597, 190, 433, 248, 188, 250, 24, 192, 222, 400, 591, 271, 16, 18, 20, 22, 399, 257, 404, 296 };
+                    List<int> springCrops = new List<int> { 597, 190, 248, 188, 250, 24, 192, 222, 400, 591, 271, 16, 18, 20, 22, 399, 296 };
+                    List<int> summerCrops = new List<int> { 258, 270, 304, 260, 254, 376, 264, 266, 268, 593, 421, 256, 262, 398, 396, 402, 259 };
+                    List<int> fallCrops   = new List<int> { 300, 274, 284, 278, 282, 272, 595, 398, 276, 280, 410, 408, 406 };
+                    List<int> winterCrops = new List<int> { 412, 414, 416, 418, 283 };
+                    List<int> springsummerCrops = new List<int> { 433 };
+                    List<int> summerfallCrops = new List<int> { 270, 421, 262 };
                     switch (Game1.currentSeason)
                     {
                         case "spring":
@@ -42,6 +47,208 @@ namespace SeasonalPricing
                                     string[] fields = data[itemID].Split('/');
                                     this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
                                     fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.6).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (fallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.4).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (winterCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (springsummerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerfallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.4).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                            }
+                            break;
+                        case "summer":
+                            this.Monitor.Log("summer", LogLevel.Debug);
+                            foreach (int itemID in data.Keys.ToArray())
+                            {
+                                if (springCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (fallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.6).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (winterCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.4).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (springsummerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerfallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                            }
+                            break;
+                        case "fall":
+                            this.Monitor.Log("fall", LogLevel.Debug);
+                            foreach (int itemID in data.Keys.ToArray())
+                            {
+                                if (springCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.4).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (fallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (winterCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.6).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (springsummerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerfallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                            }
+                            break;
+                        case "winter":
+                            this.Monitor.Log("winter", LogLevel.Debug);
+                            foreach (int itemID in data.Keys.ToArray())
+                            {
+                                if (springCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.6).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.4).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (fallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (winterCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) / 2).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (springsummerCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.4).ToString();
+                                    data[itemID] = string.Join("/", fields);
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                }
+                                if (summerfallCrops.Contains(itemID))
+                                {
+                                    string[] fields = data[itemID].Split('/');
+                                    this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
+                                    fields[1] = (int.Parse(fields[1]) * 1.2).ToString();
                                     data[itemID] = string.Join("/", fields);
                                     this.Monitor.Log($"{data[itemID]}", LogLevel.Debug);
                                 }
